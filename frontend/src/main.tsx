1c570fdef568244d78139e4f42c96180ce5bd297
Lichun <wulichun0226@gmail.com>
2025-09-03 21:58:05 +0100

    debug

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import * as React from 'react';
+console.log('React version at runtime:', React.version, 'createContext exists?', !!React.createContext);
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
