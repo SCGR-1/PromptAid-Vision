6f3f0841d06fc1f53efe532fc54528f5fbbf084d
Lichun <wulichun0226@gmail.com>
2025-09-04 17:43:43 +0100

    fix

---
+++ py_backend/static/assets/useAdmin-B_kWg5HW.js
@@ -0,0 +1,1 @@
+import{r,V as e}from"./index-C_hAuRbb.js";const o=()=>{const t=r.useContext(e);if(t===void 0)throw new Error("useAdmin must be used within an AdminProvider");return t};export{o as u};
--- py_backend/static/assets/useAdmin-C1uyaTkH.js
+++
@@ -1,1 +0,0 @@
-import{r,V as e}from"./index-Cg9DMdVA.js";const o=()=>{const t=r.useContext(e);if(t===void 0)throw new Error("useAdmin must be used within an AdminProvider");return t};export{o as u};
